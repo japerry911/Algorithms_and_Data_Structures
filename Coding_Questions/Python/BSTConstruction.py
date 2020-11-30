b8164492c32a59f655a3accacabbecfba5c7fc96
japerry911 <jack.e.perry@outlook.com>
2020-11-30 06:00:58 -0700

    📦 NEW: BST Construction boilerplate set up

---
+++ Coding_Questions/Python/BSTConstruction.py
@@ -0,0 +1,16 @@
+# ---BST Construction---
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        return self
+
+    def contains(self, value):
+        pass
+
+    def remove(self, value):
+        return self
