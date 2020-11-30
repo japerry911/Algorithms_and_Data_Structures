01e3ac83a9e0aaedcfd6b819d3f5a74f4f190ebd
japerry911 <jack.e.perry@outlook.com>
2020-11-29 21:07:27 -0700

    📦 NEW: Find Closest Value in BST python set up

---
+++ Coding_Questions/Python/FindCloestValueInBST.py
@@ -0,0 +1,12 @@
+# ---Find Closest Value in BST---
+
+
+def find_closest_value_in_bst(tree, target):
+    pass
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
