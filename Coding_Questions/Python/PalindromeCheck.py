c8edb88fee5207865857d805411a8f63c5f41be6
japerry911 <jack.e.perry@outlook.com>
2020-12-04 07:35:27 -0700

    👌 IMPROVE: improved python translation of check palindrome by make it case insensitive

--- Coding_Questions/Python/PalindromeCheck.py
+++ Coding_Questions/Python/PalindromeCheck.py
@@ -2,7 +2,7 @@
 
 
 def is_palindrome(string: str) -> bool:
-    return string == "".join(reversed(list(string)))
+    return string.lower() == "".join(reversed(list(string))).lower()
 
 
 print(is_palindrome("abcdcba"))  # True
