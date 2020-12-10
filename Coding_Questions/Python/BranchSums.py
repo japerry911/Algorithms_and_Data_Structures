dba150453ecd4cea4429cbdc094a69be6c25f266
japerry911 <jack.e.perry@outlook.com>
2020-12-10 07:55:04 -0700

    📦 NEW: Python Binary Tree BranchSums problem set up

---
+++ Coding_Questions/Python/BranchSums.py
@@ -0,0 +1,14 @@
+# ---Branch Sums---
+
+from typing import List
+
+
+class BinaryTree:
+    def __init__(self, value: int):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branch_sums(root: BinaryTree) -> int:
+    pass
