396580a67ff93d54fc74a60f1fb7e94d3e20e544
japerry911 <jack.e.perry@outlook.com>
2020-12-10 07:58:46 -0700

    📦 NEW: Python Binary Tree NodeDepths problem set up

---
+++ Coding_Questions/Python/NodeDepths.py
@@ -0,0 +1,12 @@
+# ---Node Depths---
+
+
+class BinaryTree:
+    def __init__(self, value: int):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def nodeDepths(root: BinaryTree) -> int:
+    pass
