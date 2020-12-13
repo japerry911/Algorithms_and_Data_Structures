e944e666b70d87585ed464f06329496121a89d17
japerry911 <jack.e.perry@outlook.com>
2020-12-13 08:43:29 -0700

    📦 NEW: Python translation NodeDepths problem completed

--- Coding_Questions/Python/NodeDepths.py
+++ Coding_Questions/Python/NodeDepths.py
@@ -8,5 +8,21 @@ class BinaryTree:
         self.right = None
 
 
-def nodeDepths(root: BinaryTree) -> int:
-    pass
+def node_depths(root: BinaryTree, depth: int = 0) -> int:
+    return depth + \
+           (0 if root.left is None else node_depths(root.left, depth + 1)) + \
+           (0 if root.right is None else node_depths(root.right, depth + 1))
+
+
+root_test = BinaryTree(1)
+root_test.left = BinaryTree(2)
+root_test.left.left = BinaryTree(4)
+root_test.left.left.left = BinaryTree(8)
+root_test.left.left.right = BinaryTree(9)
+root_test.left.right = BinaryTree(5)
+root_test.right = BinaryTree(3)
+root_test.right.left = BinaryTree(6)
+root_test.right.right = BinaryTree(7)
+actual = node_depths(root_test)
+print(actual)  # 16
+assert actual == 16
