2f680f71671746ac3d3fa9460b5307d80a50ff46
japerry911 <jack.e.perry@outlook.com>
2020-12-19 10:06:20 -0700

    📦 NEW: JavaScript translation DepthFirstSearch problem completed

---
+++ Coding_Questions/JavaScript/DepthFirstSearch.js
@@ -0,0 +1,21 @@
+// ---Depth-First Search---
+
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+    array.push(this.name);
+    for (const child of this.children) {
+      child.depthFirstSearch(array);
+    }
+    return array;
+  }
+}
