9f89d6a6bcc44f5de4fc3604c8608ca03352f33e
japerry911 <jack.e.perry@outlook.com>
2020-12-21 08:01:30 -0700

    👌 IMPROVE: improved Python answer to MaxSubsetSumNoAdjacent problem

--- Coding_Questions/Python/MaxSubsetSumNoAdjacent.py
+++ Coding_Questions/Python/MaxSubsetSumNoAdjacent.py
@@ -7,9 +7,7 @@ def max_subset_sum_no_adjacent(array: List[int]) -> int:
     if not len(array):
         return 0
 
-    for i in range(len(array)):
-        if i == 0 or i == 1:
-            continue
+    for i in range(2, len(array)):
         array[i] += max(array[:i-1])
 
     return max(array)
