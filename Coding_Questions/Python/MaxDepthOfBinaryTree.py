c5f1a342d414dc311f82e9fbb2f100587748d52a
japerry911 <jack.e.perry@outlook.com>
2020-12-28 13:17:57 -0700

    📦 NEW: Python translation MaxDepthOfBinaryTree problem completed

---
+++ Coding_Questions/Python/MaxDepthOfBinaryTree.py
@@ -0,0 +1,12 @@
+# ---Max Depth of Binary Tree---
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def max_depth(self, root: TreeNode, depth: int = 0) -> int:
+    return 0 if root is None else max(self.maxDepth(root.left, depth + 1),
+                                      self.maxDepth(root.right, depth)) + 1
