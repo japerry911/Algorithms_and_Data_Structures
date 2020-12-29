8446ba517c5b6191ac9c550f0e4f7dfedcfa6004
japerry911 <jack.e.perry@outlook.com>
2020-12-29 08:11:20 -0700

    📦 NEW: JavaScript translation SameBSTs compproblem completed

--- Coding_Questions/JavaScript/SameBSTs.js
+++ Coding_Questions/JavaScript/SameBSTs.js
@@ -1,12 +1,13 @@
 // ---Same BSTs---
 
 const sameBsts = (arrayOne, arrayTwo) => {
-  if (!arrayOne.length && !arrayTwo.length) {
+  if (arrayOne.length === 0 && arrayTwo.length === 0) {
     return true;
   } else if (
     arrayOne.length !== arrayTwo.length ||
     arrayOne[0] !== arrayTwo[0]
   ) {
+    console.log(arrayOne, arrayTwo);
     return false;
   } else if (arrayOne.length === 1 && arrayTwo.length === 1) {
     return arrayOne[0] === arrayTwo[0];
@@ -16,9 +17,7 @@ const sameBsts = (arrayOne, arrayTwo) => {
   const rootValue2 = arrayTwo.shift();
 
   const leftSubtreeValues1 = arrayOne.filter((element) => element < rootValue1);
-  const leftSubtreeValues2 = arrayTwo.filter(
-    (element) => element <= rootValue2
-  );
+  const leftSubtreeValues2 = arrayTwo.filter((element) => element < rootValue2);
   const rightSubtreeValues1 = arrayOne.filter(
     (element) => element >= rootValue1
   );
@@ -26,28 +25,13 @@ const sameBsts = (arrayOne, arrayTwo) => {
     (element) => element >= rootValue2
   );
 
-  if (
-    leftSubtreeValues1.sort((a, b) => a - b) !==
-    leftSubtreeValues2.sort((a, b) => a - b)
-  ) {
-    return false;
-  }
-  if (
-    rightSubtreeValues1.sort((a, b) => a - b) !==
-    rightSubtreeValues2.sort((a, b) => a - b)
-  ) {
-    return false;
-  }
-
-  console.log(rightSubtreeValues2);
-
   return (
     sameBsts(leftSubtreeValues1, leftSubtreeValues2) &&
     sameBsts(rightSubtreeValues1, rightSubtreeValues2)
   );
 };
 
-const arrayOne = [10, 15, 8, 12, 94, 81, 5, 2, 11];
-const arrayTwo = [10, 8, 5, 15, 2, 12, 11, 94, 81];
+const arrayOne = [50, 76, 81, 23, 23, 23, 100, 56, 12, -1, 3];
+const arrayTwo = [50, 23, 76, 23, 23, 12, 56, 81, -1, 3, 100];
 const actual = sameBsts(arrayOne, arrayTwo);
 console.log(actual); // true
