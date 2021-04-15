140f3ab8a5823860b08fabe1334f2fc4339e56a2
Jack Perry <jack.e.perry@outlook.com>
2021-04-15 07:20:47 -0600

    📦 NEW: Python Problem PhoneNumberMnemonics set up

---
+++ Coding_Questions/Python/PhoneNumberMnemonics.py
@@ -0,0 +1,13 @@
+# ---Phone Number Mnemonics---
+
+from typing import List
+
+
+def phone_number_mnemonics(phone_number: str) -> List[str]:
+    return []
+
+
+a = phone_number_mnemonics("1905")
+print(a)
+# ["1w0j", "1w0k", "1w0l", "1x0j", "1x0k", "1x0l", "1y0j", "1y0k", "1y0l",
+#  "1z0j", "1z0k", "1z0l"]
