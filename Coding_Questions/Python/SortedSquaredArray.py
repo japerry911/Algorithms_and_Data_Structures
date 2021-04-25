08c8813b8a0186933f07447525e04c4f0d57c599
Jack Perry <jack.e.perry@outlook.com>
2021-04-25 16:53:39 -0600

    📦 NEW: SortedSquareArray Python problem set up

---
+++ Coding_Questions/Python/SortedSquaredArray.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def sorted_square_array(array: List[int]) -> List[int]:
+    pass
+
+
+a = sorted_square_array([-9, 1, 2, 3, 4, 5, 6, 7, 8])
+print(a)
+assert a == [1, 4, 9, 16, 25, 36, 49, 64, 81]
