144aad3534ffe8efbaef7e3dc143a200e72f98fb
Jack Perry <jack.e.perry@outlook.com>
2021-05-27 22:09:35 -0600

    📦 NEW: set up problem

---
+++ Coding_Questions/Python/MinimumWaitingTime.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def minimum_waiting_time(queries: List) -> int:
+    pass
+
+
+queries_input = [3, 2, 1, 2, 6]
+expected_output = 17
+output = minimum_waiting_time(queries_input)
+print(f"output {output} == {expected_output}")
+assert output == expected_output
--- Coding_Questions/Python/NonConstructibleChange.py
+++ Coding_Questions/Python/NonConstructibleChange.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def non_constructible_change(coins: List[int]) -> int:
-    pass
+    coins_sorted = sorted(coins)
 
 
 a = non_constructible_change([5, 7, 1, 1, 2, 3, 22])
