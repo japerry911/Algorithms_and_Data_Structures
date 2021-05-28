aa4362c6b87c84e38a6a51d96f916ea622952261
Jack Perry <jack.e.perry@outlook.com>
2021-05-28 10:42:10 -0600

    📦 NEW: class photos problem set up

---
+++ Coding_Questions/Python/ClassPhotos.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def class_photos(
+        red_shirt_heights: List[int],
+        blue_shirt_heights: List[int]
+) -> bool:
+    pass
+
+
+red = [5, 8, 1, 3, 4]
+blue = [6, 9, 2, 4, 5]
+output = class_photos(red, blue)
+assert output
