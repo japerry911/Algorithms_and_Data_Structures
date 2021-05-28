dffc89f68d071bf7d9ef53b82be4648565531252
Jack Perry <jack.e.perry@outlook.com>
2021-05-28 10:39:58 -0600

    👌 IMPROVE: coin problem finished

--- Coding_Questions/Python/NonConstructibleChange.py
+++ Coding_Questions/Python/NonConstructibleChange.py
@@ -2,9 +2,19 @@ from typing import List
 
 
 def non_constructible_change(coins: List[int]) -> int:
-    coins_sorted = sorted(coins)
+    coins.sort()
+    change = 0
+
+    for coin in coins:
+        if change + 1 < coin:
+            return change + 1
+        else:
+            change += coin
+
+    return change + 1
 
 
+# [1, 1, 2, 3, 5, 7, 22]
 a = non_constructible_change([5, 7, 1, 1, 2, 3, 22])
-print(a)
+print(f"{a} == 20")
 assert a == 20
