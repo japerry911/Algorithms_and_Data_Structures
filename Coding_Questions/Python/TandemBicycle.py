c5d7af8ce7b83845fbb4961f8413c8c1be90a414
Jack Perry <jack.e.perry@outlook.com>
2021-06-02 08:05:31 -0600

    👌 IMPROVE: tandem bicycle problem completed

--- Coding_Questions/Python/TandemBicycle.py
+++ Coding_Questions/Python/TandemBicycle.py
@@ -6,14 +6,23 @@ def tandem_bicycle(
         blue_shirt_speeds: List[int],
         fastest: bool
 ) -> int:
-    combined = sorted(red_shirt_speeds + blue_shirt_speeds)
-    return sum(combined[len(red_shirt_speeds):]) if fastest is True else \
-        sum(combined[:len(red_shirt_speeds)])
+    red_shirt_speeds.sort(reverse=fastest)
+    blue_shirt_speeds.sort()
+
+    print(red_shirt_speeds)
+    print(blue_shirt_speeds)
+
+    total_speed = 0
+    for i in range(len(red_shirt_speeds)):
+        total_speed += red_shirt_speeds[i] if red_shirt_speeds[i] > \
+        blue_shirt_speeds[i] else blue_shirt_speeds[i]
+
+    return total_speed
 
 
-r_speeds = [1, 2, 1, 9, 12, 3]
-b_speeds = [3, 3, 4, 6, 1, 2]
-fastest_bool = True
+r_speeds = [5, 5, 3, 9, 2]
+b_speeds = [3, 6, 7, 2, 1]
+fastest_bool = False
 output = tandem_bicycle(r_speeds, b_speeds, fastest_bool)
-print(f"{output} == 10")
-assert output == 10
+print(f"{output} == 25")
+assert output == 25
