5cb95ed68de2dd0127eb71411a971f9977e4fd86
Jack Perry <jack.e.perry@outlook.com>
2021-06-04 15:57:41 -0600

    📦 NEW: ReverseInteger problem set up

---
+++ Coding_Questions/Python/ReverseInteger.py
@@ -0,0 +1,9 @@
+def reverse(x: int) -> int:
+    pass
+
+
+input_value = 123
+expected_output_value = 321
+output_value = reverse(input_value)
+print(output_value, "==", expected_output_value)
+assert output_value == expected_output_value
