f883c3e985b3327ef241a61f674292ca0897fac3
Jack Perry <jack.e.perry@outlook.com>
2021-06-04 16:48:26 -0600

    👌 IMPROVE: compelted ReverseInteger Problem

--- Coding_Questions/Python/ReverseInteger.py
+++ Coding_Questions/Python/ReverseInteger.py
@@ -1,9 +1,27 @@
 def reverse(x: int) -> int:
-    pass
+    start = ""
+    if 0 > x > -2_147_483_648:
+        x = abs(x)
+        start = "-"
+    elif x == 0 or x > 2_147_483_647 or x <= -2_147_483_648:
+        return 0
+
+    ret = list()
+
+    while x > 0:
+        ret.append(str(x % 10))
+        x //= 10
+
+    result = int(start + ("".join(ret)))
+
+    if result > 2_147_483_647 or result <= -2_147_483_648:
+        return 0
+    else:
+        return result
 
 
-input_value = 123
-expected_output_value = 321
+input_value = 1463847412  # -1534236470
+expected_output_value = 2147483641
 output_value = reverse(input_value)
 print(output_value, "==", expected_output_value)
 assert output_value == expected_output_value
