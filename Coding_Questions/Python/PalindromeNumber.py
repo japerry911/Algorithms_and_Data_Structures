be6f74e8c6227950e4fdf8295761608e81e7b291
Jack Perry <jack.e.perry@outlook.com>
2021-06-06 14:07:48 -0600

    👌 IMPROVE: added pytest and comments

--- Coding_Questions/Python/PalindromeNumber.py
+++ Coding_Questions/Python/PalindromeNumber.py
@@ -1,8 +1,33 @@
+import pytest
+
+
 def is_palindrome(x: int) -> bool:
+    """Determines whether an inputted integer is a Palindrome number of not
+    :param int x: the number being tested
+    :returns: True or False indicating if it is Palindromic
+    :rtype: bool
+    """
     return False
 
 
-input_int = 121
-expected_output = True
-output = is_palindrome(input_int)
-assert output == expected_output
+def test_is_palindrome():
+    print("is_palindrome(121) is True")
+    assert is_palindrome(121) is True
+
+    print("is_palindrome(-121) is False")
+    assert is_palindrome(-121) is False
+
+    print("is_palindrome(10) is False")
+    assert is_palindrome(10) is False
+
+    print("is_palindrome(-101) is False")
+    assert is_palindrome(-101) is False
+
+    print("is_palindrome(3) is True")
+    assert is_palindrome(3) is True
+
+    print("is_palindrome(9339) is True")
+    assert is_palindrome(9339) is True
+
+
+pytest.main(["PalindromeNumber.py"])
