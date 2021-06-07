34e3d518ce75d7740b7ad88946798725b1e58b66
Jack Perry <jack.e.perry@outlook.com>
2021-06-06 18:58:41 -0600

    👌 IMPROVE: RomanToInteger problem completed

--- Coding_Questions/Python/RomanToInteger.py
+++ Coding_Questions/Python/RomanToInteger.py
@@ -2,7 +2,39 @@ import pytest
 
 
 def roman_to_int(s: str) -> int:
-    pass
+    """Converts Roman Numeral to Integer
+    :param str s: Roman Numeral input
+    :returns: converted Integer
+    :rtype: int
+    """
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+        "IV": 4,
+        "IX": 9,
+        "XL": 40,
+        "XC": 90,
+        "CD": 400,
+        "CM": 900
+    }
+
+    converted_integer = 0
+    i = 0
+
+    while i < len(s):
+        if i + 1 < len(s) and s[i:i + 2] in roman_dict:
+            converted_integer += roman_dict[s[i:i + 2]]
+            i += 2
+        else:
+            converted_integer += roman_dict[s[i]]
+            i += 1
+
+    return converted_integer
 
 
 def test_roman_to_int():
