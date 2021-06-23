08878e03b4746aa323010c2e8ce5e0098fe9dd7e
Jack Perry <jack.e.perry@outlook.com>
2021-06-23 07:59:44 -0600

    👌 IMPROVE: python PlusOne problem completed

--- Coding_Questions/Python/PlusOne.py
+++ Coding_Questions/Python/PlusOne.py
@@ -3,8 +3,24 @@ from typing import List
 import pytest
 
 
-def plus_one(digits: List[int]) -> List[int]:
-    pass
+def plus_one(digits: List[int], current_idx: int = -1) -> List[int]:
+    """Given a non-empty array of decimal digits representing a non-negative
+        integer, increment one to the integer.
+    :param List[int] digits: the non negative integer input that will be
+        incremented
+    :param int current_idx: current index that is being incremented
+    :returns: incremented non negative integer input value
+    :rtype: List[int]
+    """
+    if abs(current_idx) > len(digits):
+        digits.insert(0, 1)
+    elif digits[current_idx] < 9:
+        digits[current_idx] += 1
+    else:
+        digits[current_idx] = 0
+        plus_one(digits, current_idx - 1)
+
+    return digits
 
 
 def test_plus_one():
@@ -19,6 +35,14 @@ def test_plus_one():
     digits = [0]
     print(f"plus_one({digits}) == [1]")
     assert plus_one(digits) == [1]
+
+    digits = [1, 9, 9]
+    print(f"plus_one({digits}) == [2, 0, 0]")
+    assert plus_one(digits) == [2, 0, 0]
+
+    digits = [9, 9, 9]
+    print(f"plus_one({digits}) == [1, 0, 0, 0]")
+    assert plus_one(digits) == [1, 0, 0, 0]
 
 
 pytest.main(["PlusOne.py"])
