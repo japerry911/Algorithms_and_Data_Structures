1d5e48d903800820ba0fe657769abcf0cb63a952
Jack Perry <jack.e.perry@outlook.com>
2021-06-26 11:10:49 -0600

    👌 IMPROVE: completed python remove dups from sorted list problem

--- Coding_Questions/Python/RemoveDupsFromSortedList.py
+++ Coding_Questions/Python/RemoveDupsFromSortedList.py
@@ -1,9 +1,23 @@
 class ListNode:
     def __init__(self, val=0, next=None):
-        self.val= val
+        self.val = val
         self.next = next
 
 
-def delete_duplicates(head: ListNode) -> ListNode:
-    pass
+def delete_duplicates(head: ListNode) -> ListNode or None:
+    if head is None:
+        return None
 
+    solution = ListNode(val=head.val)
+    return_solution = solution
+    previous_value = head.val
+
+    while head.next is not None:
+        head = head.next
+
+        if head.val != previous_value:
+            solution.next = ListNode(val=head.val)
+            solution = solution.next
+            previous_value = head.val
+
+    return return_solution
