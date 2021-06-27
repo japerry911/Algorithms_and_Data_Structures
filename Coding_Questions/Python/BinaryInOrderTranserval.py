318e6c8e10ceb7af9827526f972d244f40f871f8
Jack Perry <jack.e.perry@outlook.com>
2021-06-27 10:49:30 -0600

    📦 NEW: binary in order traveral python problem set up

---
+++ Coding_Questions/Python/BinaryInOrderTranserval.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def in_order_traversal(root: TreeNode) -> List[int]:
+    pass
+
+
