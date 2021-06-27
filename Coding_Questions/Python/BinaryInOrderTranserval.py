2b7d9ada7eb89fa67ecf2aa0a0275fc10dcc8032
Jack Perry <jack.e.perry@outlook.com>
2021-06-27 10:57:21 -0600

    👌 IMPROVE: in order traversal python problem completed

--- Coding_Questions/Python/BinaryInOrderTranserval.py
+++ Coding_Questions/Python/BinaryInOrderTranserval.py
@@ -8,7 +8,16 @@ class TreeNode:
         self.right = right
 
 
-def in_order_traversal(root: TreeNode) -> List[int]:
-    pass
+def in_order_traversal(root: TreeNode, return_list: List[int] = None) \
+        -> List[int]:
+    if return_list is None:
+        return_list = list()
+
+    if root is not None:
+        in_order_traversal(root.left, return_list)
+        return_list.append(root.val)
+        in_order_traversal(root.right, return_list)
+
+    return return_list
 
 
