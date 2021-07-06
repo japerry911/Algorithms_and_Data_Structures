54212259536ec2476466f42baf450014cb81ecc5
Jack Perry <jack.e.perry@outlook.com>
2021-07-06 10:12:25 -0600

    📦 NEW: Path sum problem python set up

---
+++ Coding_Questions/Python/PathSum.py
@@ -0,0 +1,9 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def has_path_sum(root: TreeNode, target_sum: int) -> bool:
+    pass
