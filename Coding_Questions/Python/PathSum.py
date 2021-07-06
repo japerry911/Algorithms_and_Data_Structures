2e4df68d99d637a73326c7b0a43488637ca1d261
Jack Perry <jack.e.perry@outlook.com>
2021-07-06 11:04:56 -0600

    👌 IMPROVE: python problem path_sum completed

--- Coding_Questions/Python/PathSum.py
+++ Coding_Questions/Python/PathSum.py
@@ -5,5 +5,24 @@ class TreeNode:
         self.right = right
 
 
-def has_path_sum(root: TreeNode, target_sum: int) -> bool:
-    pass
+def has_path_sum(root: TreeNode, target_sum: int, current_sum: int = 0) -> \
+        bool:
+    """Calculates if there a a root-to-leaf path sum for the tree and
+        target_sum
+    :param TreeNode root: the tree that is being looked through
+    :param int target_sum: the target_sum that needs to be calculated in path
+        sum
+    :param int current_sum: the current running sum of path, defaults to 0
+    :returns: returns whether a path_sum is calculate-able
+    :rtype: bool
+    """
+    if root is None:
+        return False
+    else:
+        current_sum += root.val
+
+    if current_sum == target_sum and root.left is None and root.right is None:
+        return True
+
+    return has_path_sum(root.left, target_sum, current_sum) | \
+           has_path_sum(root.right, target_sum, current_sum)
