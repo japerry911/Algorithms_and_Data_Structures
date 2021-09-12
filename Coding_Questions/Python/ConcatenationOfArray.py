eec2f4ab51e9cc73cc2f46120b8ac3ba828bb9e4
Jack Perry <jack.e.perry@outlook.com>
2021-09-12 07:52:55 -0600

    👌 IMPROVE: Problem completed - Concatenation of Array - Python

--- Coding_Questions/Python/ConcatenationOfArray.py
+++ Coding_Questions/Python/ConcatenationOfArray.py
@@ -2,10 +2,11 @@ from typing import List
 
 
 def get_concatenation(nums: List[int]) -> List[int]:
-    pass
+    return nums * 2
 
 
 nums_input = [1, 3, 2, 1]
 expected_output = [1, 3, 2, 1, 1, 3, 2, 1]
 output = get_concatenation(nums_input)
-assert nums_input == expected_output
+print(expected_output, ' == ', output)
+assert expected_output == output
