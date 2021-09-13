79adba4854a96ef6e8cea7b80bd7829db8331b54
Jack Perry <jack.e.perry@outlook.com>
2021-09-13 13:04:01 -0600

    👌 IMPROVE: monotonicArray problem completed

--- Coding_Questions/Python/MonotonicArray.py
+++ Coding_Questions/Python/MonotonicArray.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import pytest
 
@@ -9,7 +9,35 @@ def is_monotonic(array: List[int]) -> bool:
     :returns: True or False, which tells if input is monotonic or not
     :rtype: bool
     """
-    pass
+    def find_second_element(
+            input_array: List[int],
+            prev: int
+    ) -> Tuple[int, List[int]]:
+        return_element = input_array.pop(0)
+        if return_element == prev:
+            return find_second_element(input_array, prev)
+        return return_element, input_array
+
+    if len(array) <= 2:
+        return True
+
+    first_element = array.pop(0)
+
+    try:
+        second_element, array = find_second_element(array, first_element)
+    except IndexError:
+        return True
+
+    is_increasing = first_element < second_element
+    current_element = second_element
+
+    for element in array:
+        if (is_increasing and current_element > element) or \
+                (not is_increasing and current_element < element):
+            return False
+        current_element = element
+
+    return True
 
 
 def test_is_monotonic():
@@ -44,6 +72,22 @@ def test_is_monotonic():
     input_8 = [-1, -5, -10, -1100, -900, -1101, -1102, -9001]
     expected_output = False
     assert is_monotonic(input_8) == expected_output
+
+    input_9 = [1, 2, 0]
+    expected_output = False
+    assert is_monotonic(input_9) == expected_output
+
+    input_10 = [1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 7, 9, 10, 11]
+    expected_output = False
+    assert is_monotonic(input_10) == expected_output
+
+    input_11 = [1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 11]
+    expected_output = True
+    assert is_monotonic(input_11) == expected_output
+
+    input_12 = [-1, -1, -1, -1, -1, -1, -1, -1]
+    expected_output = True
+    assert is_monotonic(input_12) == expected_output
 
 
 pytest.main(["MonotonicArray"])
