749ba87e29dcd2772167de154e12be8e4990f460
Jack Perry <jack.e.perry@outlook.com>
2021-09-14 07:45:59 -0600

    📦 NEW: binary tree pre order traversal problem completed

--- Coding_Questions/Python/BinaryTreePreOrderTraversal.py
+++ Coding_Questions/Python/BinaryTreePreOrderTraversal.py
@@ -8,14 +8,14 @@ class TreeNode:
         self.right = right
 
 
-def post_order_traverse(tree: TreeNode) -> List[int]:
+def pre_order_traversal(root: TreeNode) -> List[int]:
     return_array = list()
-    return create_output(tree, return_array)
+    return create_output(root, return_array)
 
 
 def create_output(tree: TreeNode, array: List[int]) -> List[int]:
     if tree is not None:
+        array.append(tree.val)
         create_output(tree.left, array)
         create_output(tree.right, array)
-        array.append(tree.val)
     return array
