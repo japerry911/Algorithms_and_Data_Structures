e22b68f02f600949aa21c9a33848bb6d948522f8
Jack Perry <jack.e.perry@outlook.com>
2021-12-08 04:32:15 -0700

    📦 NEW: problem set up

---
+++ Coding_Questions/Python/MinNumberOfCoinsForChange.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def min_number_of_coins_for_change(n: int, denoms: List) -> int:
+    """how many coins to produce n, smallest number of coins, and use denoms
+
+    :param int n: produce this amount of change
+    :param denoms: use these as coin options
+    :rtype: int
+    :returns: smallest number of coins needed to make change
+    """
+    pass
